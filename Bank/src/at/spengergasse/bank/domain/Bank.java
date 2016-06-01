2bdaa8c8465d41cb42fda7032fe9b3a371e852d9
Lukas <lukas.koebke@gmx.at>
2016-06-01 16:35:10 +0200

    neu

---
+++ Bank/src/at/spengergasse/bank/domain/Bank.java
@@ -0,0 +1,8 @@
+package at.spengergasse.bank.domain;
+
+import java.util.*;
+
+public class Bank
+{
+private HashMap accounts;
+}
---
+++ Bank/src/at/spengergasse/bank/domain/BankAccount.java
@@ -0,0 +1,13 @@
+package at.spengergasse.bank.domain;
+
+public class BankAccount
+{
+private String name;
+private Localdate birthdate;
+private int id;
+
+	public String toString()
+	{
+		return firstname + "\t" + lastname + "\t" + age + "\t" + id;
+	}
+}
